9c70f8fc2fdc0dedcbe8b67e139f20b381110ada
Sergio Padilla <sergio.padillalopez@telefonica.com>
2019-01-31 14:13:34 +0100

    refactor code

--- Python/libraries/resource-generator/lib/yaml_parser.py
+++ Python/libraries/resource-generator/lib/yaml_parser.py
@@ -1,7 +1,11 @@
+from typing import TextIO
+
 from ruamel.yaml import YAML
+
 
 class SimpleRegex:
     yaml_tag = u'!simpleRegex'
+
     def __init__(self, def_):
         self.def_ = def_
 
@@ -13,8 +17,10 @@ class SimpleRegex:
                 def_ = value.value
         return cls(def_)
 
+
 class NestedRegex:
     yaml_tag = u'!nestedRegex'
+
     def __init__(self, def_, references):
         self.def_ = def_
         self.references = references
@@ -30,8 +36,10 @@ class NestedRegex:
                     references.append(reference.value)
         return cls(def_, references)
 
+
 class ParamsRegex:
     yaml_tag = u'!paramsRegex'
+
     def __init__(self, def_, params):
         self.def_ = def_
         self.params = params
@@ -47,8 +55,10 @@ class ParamsRegex:
                     params.append(param.value)
         return cls(def_, params)
 
+
 class Dictionary:
     yaml_tag = u'!dictionary'
+
     def __init__(self, key_type, value_type, entries):
         self.key_type = key_type
         self.value_type = value_type
@@ -66,8 +76,10 @@ class Dictionary:
                     entries[entry[0].value] = entry[1].value
         return cls(key_type, value_type, entries)
 
+
 class List:
     yaml_tag = u'!list'
+
     def __init__(self, type_, entries):
         self.type_ = type_
         self.entries = entries
@@ -83,14 +95,16 @@ class List:
                     entries.append(entry.value)
         return cls(type_, entries)
 
+
 class Char:
     yaml_tag = u'!char'
     @classmethod
     def from_yaml(cls, constructor, node):
         return node.value
 
-def parse(content: str) -> dict:
-    yaml=YAML(typ='safe')
+
+def parse(content: TextIO) -> dict:
+    yaml = YAML(typ='safe')
     yaml.register_class(SimpleRegex)
     yaml.register_class(NestedRegex)
     yaml.register_class(ParamsRegex)
