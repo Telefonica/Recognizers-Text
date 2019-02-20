99fa89db0649f20e3b395abd34ecf309959b5c07
Jesús Torres Velasco <jesus.torresvelasco@telefonica.com>
2019-02-20 10:45:05 +0100

    [[feat]] deactivated frac and nums transcription

--- Python/tests/test_runner_datetime.py
+++ Python/tests/test_runner_datetime.py
@@ -10,6 +10,7 @@ MODELFUNCTION = {
 }
 
 
+@pytest.mark.skip(reason='Not using this library')
 @pytest.mark.parametrize(
     'culture, model, options, context, source, expected_results',
     get_specs(
@@ -36,7 +37,7 @@ def test_datetime_extractor(
         simple_extractor_assert(actual, expected, 'start', 'Start')
         simple_extractor_assert(actual, expected, 'length', 'Length')
 
-
+@pytest.mark.skip(reason='Not using this library')
 @pytest.mark.parametrize(
     'culture, model, options, context, source, expected_results',
     get_specs(
@@ -82,6 +83,7 @@ def test_datetime_parser(
                 'PastResolution')
 
 
+@pytest.mark.skip(reason='Not using this library')
 @pytest.mark.parametrize(
     'culture, model, options, context, source, expected_results',
     get_specs(
@@ -119,6 +121,7 @@ def test_datetime_mergedparser(
                         assert actual_values[key] == expected_values[key]
 
 
+@pytest.mark.skip(reason='Not using this library')
 @pytest.mark.parametrize(
     'culture, model, options, context, source, expected_results',
     get_specs(
